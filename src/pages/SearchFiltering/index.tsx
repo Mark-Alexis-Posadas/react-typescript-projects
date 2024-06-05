16f9ef39800bceefee3cef91672916d7e88208df
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-06-05 10:01:08 +0800

    done search filtering

--- src/pages/SearchFiltering/index.tsx
+++ src/pages/SearchFiltering/index.tsx
@@ -29,13 +29,16 @@ const data: string[] = [
 
 const SearchFiltering: React.FC = () => {
   const [filteredItem, setFilteredItem] = useState<string>("");
+  const [visible, setVisible] = useState<boolean>(true);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFilteredItem(e.target.value);
+    setVisible(true);
   };
 
   const handleItemClick = (item: string) => {
     setFilteredItem(item);
+    setVisible(false);
   };
 
   const filteredFruits = data.filter((fruit) =>
@@ -44,6 +47,7 @@ const SearchFiltering: React.FC = () => {
 
   return (
     <div className="p-5">
+      <h1 className="text-4xl md:text-8xl font-bold">Search Fruits</h1>
       <input
         type="text"
         className="p-3 border border-slate-200 rounded w-full"
@@ -52,17 +56,23 @@ const SearchFiltering: React.FC = () => {
         onChange={handleChange}
       />
 
-      <ul className="mt-3 bg-slate-50 shadow-md p-2">
-        {filteredFruits.map((item, index) => (
-          <li
-            key={index}
-            className="border-1 border-slate-700 rounded p-2 mb-2 hover:bg-slate-300 hover:text-white cursor-pointer"
-            onClick={() => handleItemClick(item)}
-          >
-            {item}
-          </li>
-        ))}
-      </ul>
+      {visible && (
+        <ul className="mt-3 bg-slate-50 shadow-md p-2">
+          {filteredFruits.length > 0 ? (
+            filteredFruits.map((item, index) => (
+              <li
+                key={index}
+                className="border-1 border-slate-700 rounded p-2 mb-2 last:mb-0 hover:bg-slate-300 hover:text-white cursor-pointer"
+                onClick={() => handleItemClick(item)}
+              >
+                {item}
+              </li>
+            ))
+          ) : (
+            <li className="p-2 text-center">No fruits found</li>
+          )}
+        </ul>
+      )}
     </div>
   );
 };
