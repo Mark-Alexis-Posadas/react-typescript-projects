61f12a7ba5d562fb0c82d3f526f7c75dee987437
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-06-25 19:02:27 +0800

    added render component

--- src/ProjectDetails.tsx
+++ src/ProjectDetails.tsx
@@ -1,19 +1,53 @@
 import React from "react";
 import { Types } from "./data/project-list"; // Import Types interface
 import { Link } from "react-router-dom";
+import Tab from "./pages/Tab";
+import Accordion from "./pages/Accordion";
+import Carousel from "./pages/Carousel";
+import ColorPicker from "./pages/ColorPicker";
+import Counter from "./pages/Counter";
+import Ratings from "./pages/Ratings";
+import SearchFiltering from "./pages/SearchFiltering";
+import Theme from "./pages/Theme";
+import Todo from "./pages/Todo";
 
 interface ProjectDetailsProps {
   project: Types;
 }
 
 const ProjectDetails: React.FC<ProjectDetailsProps> = ({ project }) => {
+  const { title } = project;
+  const renderProjectComponent = () => {
+    switch (title) {
+      case "accordion":
+        return <Accordion />;
+      case "carousel":
+        return <Carousel />;
+      case "color-picker":
+        return <ColorPicker />;
+      case "counter":
+        return <Counter />;
+      case "ratings":
+        return <Ratings />;
+      case "search-filtering":
+        return <SearchFiltering />;
+      case "tab":
+        return <Tab />;
+      case "theme":
+        return <Theme />;
+      case "todo":
+        return <Todo />;
+      default:
+        return null;
+    }
+  };
   return (
     <div>
       <button>
         <Link to="/">Back</Link>
       </button>
       <h2>{project.title}</h2>
-      {/* Render additional details about the project */}
+      {renderProjectComponent()}
     </div>
   );
 };
