c54122811893107350a3be1f4c2cbf7f60b3243a
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-06-30 15:01:56 +0800

    added improve carousel design

---
+++ src/pages/Carousel/Carousel.tsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
+interface ActiveType {
+  active: number;
+}
+const data: string[] = [
+  "https://i.pinimg.com/564x/ba/59/2b/ba592b49ee04ddfe07bf6ce1f46964e3.jpg",
+  "https://i.pinimg.com/564x/fe/01/29/fe01293e09e5b8a55eb70369464b95aa.jpg",
+  "https://i.pinimg.com/564x/97/47/e8/9747e8bdf2737cad961f069c4c5a85b1.jpg",
+  "https://i.pinimg.com/564x/6a/24/66/6a2466c9185dbc61adbb1e9cbb0d474b.jpg",
+];
+export default function Carousel({ active }: ActiveType) {
+  const [currentSlide, setCurrentSlide] = useState<number>(active);
+  const handlePrev = () => {
+    if (currentSlide <= 0) {
+      return;
+    }
+    setCurrentSlide((currentSlide - 1 + data.length) % data.length);
+  };
+
+  const handleNext = () => {
+    if (currentSlide >= 3) {
+      return;
+    }
+    setCurrentSlide((currentSlide + 1) % data.length);
+  };
+
+  const handleDotClick = (index: number) => {
+    setCurrentSlide(index);
+  };
+
+  return (
+    <div className="mt-20 m-auto max-w-[350px] flex items-center flex-col relative bg-white shadow-md p-3 rounded">
+      {data.map((item, index) => (
+        <img
+          src={item}
+          alt="image"
+          className={`${
+            index === currentSlide ? "block" : "hidden"
+          } rounded w-[250px] object-cover h-[250px]`}
+          key={index}
+        />
+      ))}
+
+      <div className="grid grid-cols-4 w-15 gap-3 pt-3">
+        {data.map((_, index) => (
+          <div
+            key={index}
+            onClick={() => handleDotClick(index)}
+            className={`w-[10px] h-[10px] rounded-full ${
+              index === currentSlide ? "bg-green-400" : " bg-slate-300"
+            }`}
+          ></div>
+        ))}
+      </div>
+
+      <div className="flex items-center justify-between w-full absolute top-28">
+        <button
+          className={`${
+            currentSlide <= 0 ? "bg-gray-500 cursor-not-allowed" : "bg-blue-600"
+          } text-white rounded-full w-5 h-5 flex items-center justify-center p-4`}
+          onClick={handlePrev}
+        >
+          <FontAwesomeIcon icon={faArrowLeft} />
+        </button>
+        <button
+          className={`${
+            currentSlide >= 3 ? "bg-gray-500 cursor-not-allowed" : "bg-blue-600"
+          } text-white rounded-full w-5 h-5 flex items-center justify-center p-4`}
+          onClick={handleNext}
+        >
+          <FontAwesomeIcon icon={faArrowRight} />
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/pages/Carousel/index.tsx
+++ src/pages/Carousel/index.tsx
@@ -1,77 +1,17 @@
-import { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
-const data: string[] = [
-  "https://i.pinimg.com/564x/ba/59/2b/ba592b49ee04ddfe07bf6ce1f46964e3.jpg",
-  "https://i.pinimg.com/564x/fe/01/29/fe01293e09e5b8a55eb70369464b95aa.jpg",
-  "https://i.pinimg.com/564x/97/47/e8/9747e8bdf2737cad961f069c4c5a85b1.jpg",
-  "https://i.pinimg.com/564x/6a/24/66/6a2466c9185dbc61adbb1e9cbb0d474b.jpg",
-];
-export default function Carousel() {
-  const [currentSlide, setCurrentSlide] = useState<number>(0);
-  const handlePrev = () => {
-    if (currentSlide <= 0) {
-      return;
-    }
-    setCurrentSlide((currentSlide - 1 + data.length) % data.length);
-  };
+import Carousel from "./Carousel";
 
-  const handleNext = () => {
-    if (currentSlide >= 3) {
-      return;
-    }
-    setCurrentSlide((currentSlide + 1) % data.length);
-  };
-
-  const handleDotClick = (index: number) => {
-    setCurrentSlide(index);
-  };
-
+export default function Root() {
   return (
-    <div className="mt-20 m-auto max-w-[700px] flex items-center flex-col relative">
-      {data.map((item, index) => (
-        <img
-          src={item}
-          alt="image"
-          className={`${
-            index === currentSlide ? "block" : "hidden"
-          } rounded h-[500px]`}
-          key={index}
-        />
-      ))}
-
-      <div className="grid grid-cols-4 gap-3 py-3 w-full">
-        {data.map((item, index) => (
-          <img
-            src={item}
-            alt="image"
-            className={`${
-              currentSlide === index ? "" : "blur-sm"
-            } object-cover rounded w-full h-[100px] cursor-pointer`}
-            key={index}
-            onClick={() => handleDotClick(index)}
-          />
-        ))}
-      </div>
-
-      <div className="flex items-center justify-between w-full absolute top-60">
-        <button
-          className={`${
-            currentSlide <= 0 ? "bg-gray-500 cursor-not-allowed" : "bg-blue-600"
-          } text-white rounded-full w-10 h-10 flex items-center justify-center p-2`}
-          onClick={handlePrev}
-        >
-          <FontAwesomeIcon icon={faArrowLeft} />
-        </button>
-        <button
-          className={`${
-            currentSlide >= 3 ? "bg-gray-500 cursor-not-allowed" : "bg-blue-600"
-          } text-white rounded-full w-10 h-10 flex items-center justify-center p-2`}
-          onClick={handleNext}
-        >
-          <FontAwesomeIcon icon={faArrowRight} />
-        </button>
-      </div>
+    <div className="grid grid-cols-6 gap-3">
+      <Carousel active={0} />
+      <Carousel active={3} />
+      <Carousel active={1} />
+      <Carousel active={2} />
+      <Carousel active={1} />
+      <Carousel active={3} />
+      <Carousel active={0} />
+      <Carousel active={3} />
+      <Carousel active={2} />
     </div>
   );
 }
