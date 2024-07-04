9b6b830d4193cbed046f6a4b2b331eaacedfe49a
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-07-04 13:32:40 +0800

    done adding tags and group hover

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -11,9 +11,9 @@ faArrowUpRightFromSquare;
 const Projects: React.FC = () => {
   return (
     <nav>
-      <ul className="grid md:grid-cols-2 lg:grid-cols-3 gap-3">
+      <ul className="grid md:grid-cols-2 lg:grid-cols-3 gap-3 ">
         {projectList.map((project: Types) => (
-          <li key={project.id} className="bg-white p-3 rounded">
+          <li key={project.id} className="bg-white p-3 rounded group">
             <Link to={`/${project.title}`}>
               <header className="flex items-center justify-between py-2">
                 <FontAwesomeIcon
@@ -31,10 +31,20 @@ const Projects: React.FC = () => {
                   />
                 </div>
               </header>
-              <h2 className="font-bold text-[#999] capitalize mb-2">
+              <h2 className="font-bold text-[#999] capitalize">
                 {project.title}
               </h2>
-              <p className="text-[#666] text-sm">{project.description}</p>
+              <p className="text-[#666] text-sm my-2">{project.description}</p>
+              <ul className="flex items-center gap-3">
+                {project.tags.map((item, index) => (
+                  <li
+                    key={index}
+                    className="group-hover:bg-green-200 group-hover:text-white text-sm text-green-500 border border-green-500 py-1 px-2 bg-green-100 rounded-full flex items-center justify-center p-1"
+                  >
+                    {item}
+                  </li>
+                ))}
+              </ul>
             </Link>
           </li>
         ))}
